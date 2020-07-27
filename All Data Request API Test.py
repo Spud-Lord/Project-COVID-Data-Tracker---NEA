19e7768070659001d754a09f3f35fcacb475f850
Jake Eaton <spud-lord@outlook.com>
2020-07-27 12:08:02 +0100

    More Testing
    Country Specific API Test
    Copied in actual code that the Website has to see if code was improved

--- All Data Request API Test.py
+++ All Data Request API Test.py
@@ -1,6 +1,17 @@
 #Call All Data Test
 
+<<<<<<< Updated upstream
 import requests
 
+url = "https://api.covid19api.com/all"
+
+payload = {}
+headers= {}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+=======
 response = requests.get("https://api.covid19api.com/all")
 print(response.content)
+>>>>>>> Stashed changes
