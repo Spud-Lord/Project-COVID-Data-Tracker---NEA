0285d1dd075084543fc190af15b1c0fc08850878
Jake Eaton <spud-lord@outlook.com>
2020-07-27 13:10:46 +0100

    Added Official and Non-Official Version

--- Country Specific API Test.py
+++ Country Specific API Test.py
@@ -2,11 +2,6 @@
 
 import requests
 
-url = "https://api.covid19api.com/country/south-africa/status/confirmed"
+url = requests.get("https://api.covid19api.com/country/south-africa/status/confirmed")
 
-payload = {}
-headers= {}
-
-response = requests.request("GET", url, headers=headers, data = payload)
-
-print(response.text.encode('utf8'))
+print(url.content)
