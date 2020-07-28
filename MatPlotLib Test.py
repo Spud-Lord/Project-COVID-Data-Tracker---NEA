d787979ccd02073ae197b7aabbfa5b99e621d68b
Jake Eaton <spud-lord@outlook.com>
2020-07-28 13:31:46 +0100

    Creation and Commenting
    Created calldatafunction.py
    Commented out all MatPlotLib Test

--- MatPlotLib Test.py
+++ MatPlotLib Test.py
@@ -13,16 +13,16 @@ import matplotlib.pyplot as plt
 # plt.axis([0, 6, 0, 20]) #Lists the axis numbers - The first two are for the X Axis, the second two are for the Y Axis
 # plt.show() #Show Graph
 
-names = ['group_a', 'group_b', 'group_c'] #States Name for different graphs
-values = [1, 10, 100] #Sets values for points
-
-plt.figure(figsize=(9, 3)) #States size of graphs
-
-plt.subplot(131)
-plt.bar(names, values) #Plots a bar graph with names and values
-plt.subplot(132)
-plt.scatter(names, values) #Plots a scatter graph with names and values
-plt.subplot(133)
-plt.plot(names, values) #Plots a line graph with names and values
-plt.suptitle('Categorical Plotting') #Nmae of document
-plt.show() #Show Graph
+# names = ['group_a', 'group_b', 'group_c'] #States Name for different graphs
+# values = [1, 10, 100] #Sets values for points
+#
+# plt.figure(figsize=(9, 3)) #States size of graphs
+#
+# plt.subplot(131)
+# plt.bar(names, values) #Plots a bar graph with names and values
+# plt.subplot(132)
+# plt.scatter(names, values) #Plots a scatter graph with names and values
+# plt.subplot(133)
+# plt.plot(names, values) #Plots a line graph with names and values
+# plt.suptitle('Categorical Plotting') #Nmae of document
+# plt.show() #Show Graph
---
+++ calldatafunction.py
@@ -0,0 +1,9 @@
+#Jake Eaton
+
+
+import requests
+
+def Call():
+    country = country.lower()
+    url = requests.get("https://api.covid19api.com/country/"+country+"/status/confirmed")
+    
