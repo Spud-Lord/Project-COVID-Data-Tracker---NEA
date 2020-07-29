0cb26718180a17d9c10b89ffd1547210d5ff59be
Jake Eaton <spud-lord@outlook.com>
2020-07-29 15:27:47 +0100

    .

--- calldatafunction.py
+++ calldatafunction.py
@@ -6,4 +6,4 @@ import requests
 def Call():
     country = country.lower()
     url = requests.get("https://api.covid19api.com/country/"+country+"/status/confirmed")
-    
+    print(country.content)
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 #Jake Eaton
 
 from calldatafunction import Call
+
+global country
+
+country = input("Input here: ")
+Call()
