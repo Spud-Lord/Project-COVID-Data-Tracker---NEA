4c1db652f462523e0513a503fd79bc5f9f298151
Jake Eaton <spud-lord@outlook.com>
2020-08-03 14:22:33 +0100

    Update calldatafunction.py
    Import typing
    Modified Text to be typing

--- calldatafunction.py
+++ calldatafunction.py
@@ -1,10 +1,11 @@
 #Jake Eaton
 
-
+from typing import type. type2
 import requests
 
 def CountryCall():
-    country = input("What country do you want data from? ")
+    type("What country do you want data from? For countries with multiple words in their name, put a - inbetween each word e.g Write South Africa as South-Africa")
+    country = input("")
     country = country.lower()
     url = requests.get("https://api.covid19api.com/country/"+country+"/status/confirmed")
     print(url.content)
