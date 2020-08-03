892f0a427f9f21f688746abc42dc6fbb82a584b5
Jake Eaton <spud-lord@outlook.com>
2020-08-03 12:07:17 +0100

    Create navigation.py
    Created Naivgation Program
    Laid out main Framework

---
+++ navigation.py
@@ -0,0 +1,40 @@
+#Jake Eaton
+
+from creategdpgraph import GDPGraph
+from createinflationgraph import InflationGraph
+from createinfectiongraph import InfectionGraph
+from createdeathgraph import DeathGraph
+from typing import type, type2
+import time
+
+def Nav():
+    type("Would you like to view a Financial Graph or COVID-19 Data Graph? Type Exit to go back")
+    choice = input("")
+    print("")
+
+    if choice.lower() == "financial":
+        type("Would you like to view GDP or Inflation?")
+        fgraphtype = input("")
+
+        if fgraphtype.lower() == "gdp":
+            GDPGraph()
+
+        elif fgraphtype.lower() == "inflation":
+            InflationGraph()
+
+    elif choice.lower() == "covid-19":
+        type("Would you like to view Infection or Death numbers?")
+        cgraphtype = input("")
+
+        if cgraphtype.lower() == "infection":
+            InfectionGraph()
+
+        elif cgraphtype.lower() == "death":
+            DeathGraph()
+
+    elif choice.lower() == "exit":
+        print("To be Added")
+
+    else:
+        type("Please choose one of the options above")
+        time.sleep(2)
