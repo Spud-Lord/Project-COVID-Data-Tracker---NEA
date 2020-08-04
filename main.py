be5c4686ac163fd94588444ca37f599d3e65f251
Jake Eaton <spud-lord@outlook.com>
2020-08-04 20:32:28 +0100

    Update main.py
    Removed useless import

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #Jake Eaton
 
-from calldatafunction import CountryCall
 from typing import type, type2 #Imports functions from programs
 from navigation import Nav
 import time #Imports Time Module
