ac3330594a6e59762b2e25a28259986196f2aacd
Jake Eaton <spud-lord@outlook.com>
2020-08-10 13:46:31 +0100

    Update typing.py
    Updated Comments

--- typing.py
+++ typing.py
@@ -7,13 +7,13 @@ import time
 def type(string):
     for char in string:
         sys.stdout.write(char)
-        sys.stdout.flush()          #This def will print strings in this way. When type is used instead of print, the program will now know to print each character separately wth a small time gap inbetween
+        sys.stdout.flush()          #This def will print strings in this way. When type is used instead of print, the program will now know to print each character separately wth a tiny time gap inbetween
         time.sleep(0.04)
     sys.stdout.write("\n")
 
 def type2(string):
     for char in string:
         sys.stdout.write(char)
-        sys.stdout.flush()          #This def will print strings in this way. When type is used instead of print, the program will now know to print each character separately wth a small time gap inbetween
+        sys.stdout.flush()          #This def will print strings in this way. When type is used instead of print, the program will now know to print each character separately wth a tiny time gap inbetween
         time.sleep(0.75)
     sys.stdout.write("\n")
