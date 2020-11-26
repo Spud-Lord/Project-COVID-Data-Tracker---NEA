ba7d4f41e6897aa969442f6638571d6ae01a1fbf
Jake Eaton <spud-lord@outlook.com>
2020-11-26 12:11:56 +0000

    Update dataformatter.py
    Spaced out code to make it easier to read

--- dataformatter.py
+++ dataformatter.py
@@ -4,9 +4,11 @@ def Formatter(data):
     quotes = 0
     important = False   #Sets the boolean important as False
     word = ""   #Sets word as blank
+    
     for i in range(len(data)):  #For loop to set data batches
         if data[i] == "{":#New data batch
             batch = []  #Sets array called batch
+
         elif data[i] == "}":#End of batch
             SortedData.append(batch)    #Adds the batch data to the Sorted Data Array
 
@@ -17,18 +19,25 @@ def Formatter(data):
         elif important == True:
             if data[i] == ",":
                 important = False   #Sets boolean important as False if the requirements are met
+
                 if word == "":
                     word = None #If word is blank then set the word variable as nothing
+
                 try:
                     word = float(word)  #If the above is not met then try to make the variable word as a float
+
                 except:
                     pass    #If nothing is met then pass it
+
                 try:
                     word = int(word)    #Try making word an integer
+
                 except:
                     pass    #Pass it if not met
+
                 batch.append(word)  #Appends the batch Array with the word variable
                 word = ""   #Sets word to be blank
+
             else:
                 if data[i] != '"':
                     word = word + data[i]   #Sets word variable to be word with the data[i] added to the end
