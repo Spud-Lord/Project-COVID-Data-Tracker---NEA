5fd7c0b81d72d05e82a50e23a260aca7b410ef21
Jake Eaton <spud-lord@outlook.com>
2020-12-02 13:08:06 +0000

    Update main.py
    Added in Comment and MacOS and Linux Clear command

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ import matplotlib.pyplot as plt #Imports MatPlotLib Module
 from os import system, name #Imports the system and name elements from OS Module
 
 def clear(): #Defines the clear command for the Terminal - Utilizes the built in "cls" command in Windows Command Prompt to clear the screen in Python
-    if name =='nt': #Currently only set up for Windows
+    if name =='nt': #Windows ("NT" mean New Technology which was based off Windows 95)
         _ = system('cls')
+
+    else:   #MacOS and Linux
+        _ = system('clear')
 
 type("Welcome!\n")
 time.sleep(2)
